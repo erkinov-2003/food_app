03218996c3eb04f92d8e6639b68d1090a427e7fe
Localcoder <wiut00011625@gmail.com>
2023-07-23 00:02:04 +0500

    small changes

---
+++ lib/src/screens/photo_screen/photo_scanner.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:get_it/get_it.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  late List<CameraDescription> cameras;
+  late CameraController cameraController;
+
+  int direction = 0;
+
+  @override
+  void initState() {
+    startCamera(direction);
+    super.initState();
+  }
+
+  void startCamera(int direction) async {
+    cameras = await availableCameras();
+
+    cameraController = CameraController(
+      cameras[direction],
+      ResolutionPreset.high,
+      enableAudio: false,
+    );
+
+    await cameraController.initialize().then((value) {
+      if(!mounted) {
+        return;
+      }
+      setState(() {}); //To refresh widget
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  @override
+  void dispose() {
+    cameraController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(cameraController.value.isInitialized) {
+      return Scaffold(
+        body: Stack(
+          children: [
+            CameraPreview(cameraController),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  direction = direction == 0 ? 1 : 0;
+                  startCamera(direction);
+                });
+              },
+              child: button(Icons.flip_camera_ios_outlined, Alignment.bottomLeft),
+            ),
+            GestureDetector(
+              onTap: () {
+                cameraController.takePicture().then((XFile? file) {
+                  if(mounted) {
+                    if(file != null) {
+                      print("Picture saved to ${file.path}");
+                    }
+                  }
+                });
+              },
+              child: button(Icons.camera_alt_outlined, Alignment.bottomCenter),
+            ),
+            Align(
+              alignment: AlignmentDirectional.topCenter,
+              child: Text(
+                "My Camera",
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    } else {
+      return const SizedBox();
+    }
+  }
+
+  Widget button(IconData icon, Alignment alignment) {
+    return Align(
+      alignment: alignment,
+      child: Container(
+        margin: const EdgeInsets.only(
+          left: 20,
+          bottom: 20,
+        ),
+        height: 50,
+        width: 50,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(2, 2),
+              blurRadius: 10,
+            ),
+          ],
+        ),
+        child: Center(
+          child: Icon(
+            icon,
+            color: Colors.black54,
+          ),
+        ),
+      ),
+    );
+  }
+
+}
+
+class CameraService {
+  CameraController? _cameraController;
+  CameraController? get cameraController => _cameraController;
+
+  Future<void> initialize() async {
+    if (_cameraController != null) return;
+    CameraDescription description = await _getCameraDescription();
+    await _setupCameraController(description: description);
+  }
+
+  Future<CameraDescription> _getCameraDescription() async {
+    List<CameraDescription> cameras = await availableCameras();
+    return cameras.firstWhere((CameraDescription camera) =>
+    camera.lensDirection == CameraLensDirection.front);
+  }
+
+  Future _setupCameraController({
+    required CameraDescription description,
+  }) async {
+    _cameraController = CameraController(
+      description,
+      ResolutionPreset.high,
+      enableAudio: false,
+    );
+    await _cameraController?.initialize();
+  }
+
+  dispose() async {
+    await _cameraController?.dispose();
+    _cameraController = null;
+  }
+}
+
+final locator = GetIt.instance;
+
+void setupLocator() {
+  locator.registerLazySingleton<CameraService>(() => CameraService());
+}
