237f23b75a11e7a5fdac56aa5ee23a803cd9daeb
Localcoder <wiut00011625@gmail.com>
2023-07-23 21:18:45 +0500

    minor changes and updates

--- lib/src/screens/photo_screen/photo_scanner.dart
+++ lib/src/screens/photo_screen/photo_scanner.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:foodapp/src/constants/imagePath.dart';
+import 'package:foodapp/src/screens/photo_screen/zoom_screen.dart';
 import 'package:get_it/get_it.dart';
 
 import 'result_page.dart';
@@ -14,9 +15,10 @@ class PhotoScreen extends StatefulWidget {
 
 class _MainScreenState extends State<PhotoScreen> {
   late List<CameraDescription> cameras;
-  late CameraController cameraController;
+  late CameraController? cameraController;
 
   int direction = 0;
+  bool isCameraReady = false;
 
   @override
   void initState() {
@@ -24,7 +26,7 @@ class _MainScreenState extends State<PhotoScreen> {
     super.initState();
   }
 
-  void startCamera(int direction) async {
+  Future<void> startCamera(int direction) async {
     cameras = await availableCameras();
 
     cameraController = CameraController(
@@ -33,133 +35,150 @@ class _MainScreenState extends State<PhotoScreen> {
       enableAudio: false,
     );
 
-    await cameraController.initialize().then((value) {
+    await cameraController!.initialize().then((value) {
       if (!mounted) {
         return;
       }
-      setState(() {}); //To refresh widget
+      setState(() {
+        isCameraReady = true;
+      });
     }).catchError((e) {});
   }
 
   @override
   void dispose() {
-    cameraController.dispose();
+    cameraController?.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (cameraController.value.isInitialized) {
-      return Scaffold(
-        backgroundColor: Colors.white,
-        body: Stack(
-          children: [
-            CameraPreview(cameraController),
-            const Align(
-              alignment: Alignment.topRight,
-              child: Padding(
-                padding: EdgeInsets.only(right: 20, top: 50),
-                child: Icon(
-                  Icons.flash_auto_rounded,
-                  color: Colors.white,
-                  size: 30,
-                ),
+    if (!isCameraReady) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          CameraPreview(cameraController!),
+          const Align(
+            alignment: Alignment.topRight,
+            child: Padding(
+              padding: EdgeInsets.only(right: 20, top: 50),
+              child: Icon(
+                Icons.flash_auto_rounded,
+                color: Colors.white,
+                size: 30,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, top: 45),
-              child: IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, top: 45),
+            child: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.clear,
+                color: Colors.white,
+                size: 35,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 30, bottom: 70),
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    direction = direction == 0 ? 1 : 0;
+                    startCamera(direction);
+                  });
                 },
-                icon: const Icon(
-                  Icons.clear,
-                  color: Colors.white,
-                  size: 35,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 30, bottom: 70),
-              child: Align(
-                alignment: Alignment.bottomRight,
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      direction = direction == 0 ? 1 : 0;
-                      startCamera(direction);
-                    });
-                  },
-                  child: SizedBox(
-                    height: 50,
-                    width: 50,
-                    child: DecoratedBox(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        borderRadius: BorderRadius.circular(12),
-                        color: const Color(0xFFFFF8EE),
-                      ),
-                      child: const Image(
-                        image: AssetImage(ImagePath.flip),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 50),
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: FilledButton(
-                  style: FilledButton.styleFrom(
-                    backgroundColor: const Color(0xFFFFC0B7),
-                    padding: const EdgeInsets.all(18),
-                    fixedSize: const Size(85, 85),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(100),
-                    ),
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const ResultPage(),
-                      ),
-                    );
-                  },
-                  child: const CircleAvatar(
-                    radius: 100,
-                    backgroundColor: Color(0xFFFF8473),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 30, bottom: 70),
-              child: Align(
-                alignment: Alignment.bottomLeft,
                 child: SizedBox(
                   height: 50,
                   width: 50,
                   child: DecoratedBox(
                     decoration: BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        borderRadius: BorderRadius.circular(15),
-                        image: const DecorationImage(
-                          image: AssetImage(ImagePath.cooking),
-                          fit: BoxFit.fill,
-                        )),
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(12),
+                      color: const Color(0xFFFFF8EE),
+                    ),
+                    child: const Image(
+                      image: AssetImage(ImagePath.flip),
+                    ),
                   ),
                 ),
               ),
-            )
-          ],
-        ),
-      );
-    } else {
-      return const SizedBox();
-    }
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 50),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: FilledButton(
+                style: FilledButton.styleFrom(
+                  backgroundColor: const Color(0xFFFFC0B7),
+                  padding: const EdgeInsets.all(18),
+                  fixedSize: const Size(85, 85),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const ResultPage(),
+                    ),
+                  );
+                },
+                child: const CircleAvatar(
+                  radius: 100,
+                  backgroundColor: Color(0xFFFF8473),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30, bottom: 70),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const ZoomScreen(),
+                    ),
+                  );
+                },
+                child: SizedBox(
+                  height: 50,
+                  width: 50,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(15),
+                      image: const DecorationImage(
+                        image: AssetImage(ImagePath.cooking),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
 
@@ -190,7 +209,7 @@ class CameraService {
     await _cameraController?.initialize();
   }
 
-  dispose() async {
+  void dispose() async {
     await _cameraController?.dispose();
     _cameraController = null;
   }
@@ -201,5 +220,3 @@ final locator = GetIt.instance;
 void setupLocator() {
   locator.registerLazySingleton<CameraService>(() => CameraService());
 }
-
-
